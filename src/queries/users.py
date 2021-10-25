46e777decfc2041f6c35ae9c234a8854bcd65b3d
Ade Balogun <abalogun316@gmail.com>
2021-10-24 23:15:21 -0400

    terminal quick dashboard on user stats

--- src/queries/users.py
+++ src/queries/users.py
@@ -1,7 +1,8 @@
 from datetime import datetime, date, timedelta
 
-from blubber_orm import Users, Orders
+from blubber_orm import Users, Orders, Tags, Items
 
+from .reservations import get_conversion_ratio_user
 from .utils import data_to_csv
 
 def get_renters_by_date_placed(date_placed):
@@ -15,3 +16,62 @@ def get_renters_by_date_placed(date_placed):
     targets = set(targets)
     data_to_csv(targets)
     return targets
+
+def get_value_to_date(user_id):
+    renter = Users.get(user_id)
+    orders = Orders.filter({"renter_id": user_id})
+    total_num_of_orders = 0
+    total_value_of_orders = 0.0
+
+    conversion_ratio = get_conversion_ratio_user(user_id)
+
+    is_renting = False
+    favorites = {}
+
+    for order in orders:
+        total_num_of_orders += 1
+        total_value_of_orders += order.reservation._charge
+
+        if order.res_date_start < date.today():
+            if order.ext_date_end > date.today():
+                is_renting = True
+
+        item = Items.get(order.item_id)
+        tags = Tags.by_item(item)
+        for tag in tags:
+            if favorites.get(tag.name) is None:
+                favorites[tag.name] = 1
+            else:
+                favorites[tag.name] += 1
+
+    favorite_item = "all"
+    favorite_count = 0
+    for tag_name, count in favorites.items():
+        if count > favorite_count and tag_name != "all":
+            favorite_count = count
+            favorite_item = tag_name
+
+    user_since = renter.dt_joined.strftime("%B %-d, %Y")
+    joined_days_ago = (date.today() - renter.dt_joined.date()).days
+
+    print(
+    f"""
+        +++++++++++++++++++++++++++++++++++++++++++++++
+
+        Hubbub Shop Stats, User: {user_id}
+
+            Renter: {renter.name}
+            User Since: {user_since}, {joined_days_ago} days ago...
+
+            Orders-to-Date: {total_num_of_orders}
+            Lifetime Value: ${total_value_of_orders}
+
+            Conversion Ratio: {conversion_ratio}
+
+            Most Rented Category: {favorite_item}
+            Is Currently Renting: {is_renting}
+
+        +++++++++++++++++++++++++++++++++++++++++++++++
+
+    """
+    )
--- src/run.py
+++ src/run.py
@@ -1,12 +1,6 @@
 from time import sleep
 from manage import swap_order, cancel_order
+from queries import get_value_to_date
 
-swap_order(order_id="75", swap_item_id="156")
-
-"""
-renter_id="2787",
-item_id="264",
-date_started="2021-10-15",
-date_ended="2021-12-15",
-new_item_id="265"
-"""
+user_id = input("Which user id would you like stats on? ")
+get_value_to_date(user_id=user_id)
