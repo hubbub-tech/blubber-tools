31de3712fd0f4338e0327e9f11416d10fabe8347
angelhe <angelhe903@gmail.com>
2021-11-23 21:07:24 -0500

    add file for user ltv calculation

---
+++ src/blubber_tools/queries/ltv.py
@@ -0,0 +1,61 @@
+# customer life time value calculation
+# only doing revenue for now, not accounting for costs or margins
+# formula: average order value per user * average number of orders per year per user * number of years a user stays with us
+# (1 order = 1 item rental, because do not have data on check out carts or combining items in check out)
+# sources:
+# 1) https://www.netsuite.com/portal/resource/articles/ecommerce/customer-lifetime-value-clv.shtml
+# 2) https://corporatefinanceinstitute.com/resources/knowledge/valuation/lifetime-value-calculation/
+
+import pandas as pd
+import numpy as np
+from metrics import write_reservations_to_pandas, write_orders_to_pandas, write_users_to_pandas
+
+res=write_reservations_to_pandas()
+res_cal=res[res.is_calendared==1]
+orders=write_orders_to_pandas()
+users=write_users_to_pandas()
+
+# two ways to do ltv: 1) only using data from users who order, 2) using data from all users, including ones who haven't ordered
+
+# 1) only using data from ordering users (higher ltv)
+# avg order value per user:
+avg_order_val_1=np.mean(res_cal.groupby('renter_id')['charge'].sum()/res_cal.groupby('renter_id').size())
+# avg num orders per user per year, using data from sep 2020 to sep 2021:
+orders_1yr=orders[(orders.date_placed.astype(str)>='2020-09-01')&(orders.date_placed.astype(str)<='2021-09-30')]
+avg_num_orders_1=np.mean(orders_1yr.groupby('renter_id').size())
+# num years a user stays with us - tbd:
+# can do 4 years (length of undergrad)
+# or 3 years (length of undergrad - 1 year for discovering hubbub)
+# or 3.5 years (length of undergrad - 0.5 year for discovering hubbub)
+# what about master, phd students?
+# can we use data to estimate?
+def ltv_ordering_users(len_retention): # input: number of years a user stays with us
+    return avg_order_val_1 * avg_num_orders_1 * len_retention
+
+print("ltv for ordering users, assuming a user stays with us for 3 years: ",ltv_ordering_users(3))
+print("ltv for ordering users, assuming a user stays with us for 3.5 years: ",ltv_ordering_users(3.5))
+print("ltv for ordering users, assuming a user stays with us for 4 years: ",ltv_ordering_users(4))
+
+# 2) using data from all users (conservative ltv):
+# avg order value per user:
+non_ordering_users=users[~(users.id.isin(list(orders_1yr.renter_id)))]
+res_cal_non_ordering=pd.DataFrame()
+for i in non_ordering_users.id:
+  res_cal_non_ordering=pd.concat([res_cal_non_ordering,pd.DataFrame([np.nan,np.nan,np.nan,i,0,np.nan]).T])
+res_cal_non_ordering.columns=(list(res_cal.columns))
+res_cal_all=pd.concat([res_cal,res_cal_non_ordering])
+avg_order_val_2=np.mean(res_cal_all.groupby('renter_id')['charge'].sum()/res_cal_all.groupby('renter_id').size())
+# avg num orders per user per year:
+orders_1yr_non_ordering=pd.DataFrame()
+for i in non_ordering_users.id:
+  orders_1yr_non_ordering=pd.concat([orders_1yr_non_ordering,pd.DataFrame([np.nan,i,np.nan,np.nan]).T])
+orders_1yr_non_ordering.columns=(list(orders_1yr.columns))
+orders_1yr_all=pd.concat([orders_1yr,orders_1yr_non_ordering])
+avg_num_orders_2=np.mean(orders_1yr_all.groupby('renter_id').count().iloc[:,0])
+# ltv:
+def ltv_all_users(len_retention): # input: number of years a user stays with us
+    return avg_order_val_2 * avg_num_orders_2 * len_retention
+    
+print("ltv for all users, assuming a user stays with us for 3 years: ",ltv_all_users(3))
+print("ltv for all users, assuming a user stays with us for 3.5 years: ",ltv_all_users(3.5))
+print("ltv for all users, assuming a user stays with us for 4 years: ",ltv_all_users(4))
