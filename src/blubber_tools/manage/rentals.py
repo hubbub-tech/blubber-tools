05e3025bc3e126a312e15bd8a43140447b3f27fb
Ade Balogun <abalogun316@gmail.com>
2022-01-12 20:20:23 -0500

    local edits

--- src/blubber_tools/manage/rentals.py
+++ src/blubber_tools/manage/rentals.py
@@ -9,6 +9,28 @@ from blubber_orm import Orders, Extensions, Reservations
 
 from .utils import _safely_swap_reservation
 
+def exp_decay(retail_price, time_now, discount=.50, time_total=28):
+    #Where discount is issued at time_total
+    base_price = retail_price / 10
+    if time_now > 180:
+        time_total = 56
+    compound = retail_price / 90
+    a = compound * 10 ** (-log((1 - discount), 10) / (time_total - 1))
+    r = 1 - (compound / a)
+    y = a * (1 - r) ** time_now #per_day_price_now
+    #calculate the cost of the rental to the user
+    integ_time = y / log(1 - r)
+    integ_0 = a * (1 - r) / log(1 - r)
+    cost_to_date = base_price + integ_time - integ_0
+    if cost_to_date < base_price:
+        return base_price
+    return cost_to_date
+
+def check_price(duration):
+    cost = exp_decay(retail_price=95.00, time_now=duration)
+    discounted_cost = cost * 0.65
+    return discounted_cost
+
 def swap_order(order_id, swap_item_id):
     order = Orders.get(order_id)
     if order:
